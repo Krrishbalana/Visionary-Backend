55c25c2e6a704d5c5a130c784baa179a305b76db
Krrishbalana <krrishbalana@gmail.com>
2025-08-23 18:54:46 +0530

    Utilities code

---
+++ src/utils/asyncHandlers.js
@@ -0,0 +1,27 @@
+const asyncHandlers =  (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+export { asyncHandlers }
+
+
+/*
+-> 2nd way to write the function
+
+const asyncHandlers = (fn) => {
+    async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            res.status(error.code || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
+}
+*/
