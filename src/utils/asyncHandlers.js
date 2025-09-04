3129cc4ff0e6a2a448a461381a922f0e698b225a
Krrishbalana <krrishbalana@gmail.com>
2025-09-04 17:06:37 +0530

    async handler formating

--- src/utils/asyncHandlers.js
+++ src/utils/asyncHandlers.js
@@ -7,18 +7,18 @@ const asyncHandlers = (requestHandler) => {
 export { asyncHandlers };
 
 /*
--> 2nd way to write the function
-
-const asyncHandlers = (fn) => {
-    async (req, res, next) => {
-        try {
-            await fn(req, res, next)
-        } catch (error) {
-            res.status(error.code || 500).json({
-                success: false,
-                message: error.message
-            })
-        }
-    }
-}
-*/
+  -> 2nd way to write the function
+  
+  const asyncHandlers = (fn) => {
+      async (req, res, next) => {
+          try {
+              await fn(req, res, next)
+          } catch (error) {
+              res.status(error.code || 500).json({
+                  success: false,
+                  message: error.message
+              })
+          }
+      }
+  }
+  */
