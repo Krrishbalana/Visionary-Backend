246e1fb4070e82ec1d8899fb0484b46ac9bcf104
Krrishbalana <krrishbalana@gmail.com>
2025-09-08 23:58:13 +0530

    add comment and dashboard controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,18 +8,68 @@ const getVideoComments = asyncHandlers(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+  if (!videoId) {
+    throw new ApiError(400, "Video ID is required");
+  }
+  const comments = await Comment.find({ video: videoId })
+    .populate("user", "username avatar fullname")
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * limit)
+    .limit(parseInt(limit));
+
+  const totalComments = await Comment.countDocuments({ video: videoId });
+  return res.status(200).json(
+    new ApiResponse(true, "Comments fetched successfully", {
+      comments,
+      totalComments,
+      currentPage: parseInt(page),
+      totalPages: Math.ceil(totalComments / limit),
+    }),
+  );
 });
 
 const addComment = asyncHandlers(async (req, res) => {
   // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+  if (!videoId) {
+    throw new ApiError(400, "Video ID is required");
+  }
+  if (!content) {
+    throw new ApiError(400, "Comment content is required");
+  }
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user._id,
+  });
+  return res
+    .status(201)
+    .json(new ApiResponse(true, "Comment added successfully", comment));
 });
 
 const updateComment = asyncHandlers(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+  if (!commentId) {
+    throw new ApiError(400, "Comment ID is required");
+  }
+  if (!content) {
+    throw new ApiError(400, "Comment content is required");
+  }
 });
 
 const deleteComment = asyncHandlers(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+  if (!commentId) {
+    throw new ApiError(400, "Comment ID is required");
+  }
+  await Comment.findOneAndDelete({ _id: commentId, owner: req.user._id });
+  return res
+    .status(200)
+    .json(new ApiResponse(true, "Comment deleted successfully", null));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,12 +8,59 @@ import { asyncHandlers } from "../utils/asyncHandlers.js";
 
 const getChannelStats = asyncHandlers(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-  const user = req.user;
-  console.log(user);
+  const userId = req.user._id;
+  console.log(userId);
+
+  // total video uploaded by the channel
+  const totalVideos = await Video.countDocuments({ owner: userId });
+
+  //total views on all videos uploaded by the channel
+  const totalViewsAggregate = await Video.aggregate([
+    { $match: { owner: new mongoose.Types.ObjectId(userId) } },
+    { $group: { _id: null, totalViews: { $sum: "$views" } } },
+  ]);
+
+  // total likes on all videos uploaded by the channel
+  const totalLikesAggregate = await Like.aggregate([
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "videoDetails",
+      },
+    },
+    { $unwind: "$videoDetails" },
+    {
+      $match: { "videoDetails.owner": new mongoose.Types.ObjectId(userId) },
+    },
+    { $count: "totalLikes" },
+  ]);
+
+  // total subscribers of the channel
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: userId,
+  });
+
+  return res.status(200).json(
+    new ApiResponse(true, "Channel stats fetched successfully", {
+      totalVideos,
+      totalViews:
+        totalViewsAggregate.length > 0 ? totalViewsAggregate[0].totalViews : 0,
+      totalLikes:
+        totalLikesAggregate.length > 0 ? totalLikesAggregate[0].totalLikes : 0,
+      totalSubscribers,
+    }),
+  );
 });
 
 const getChannelVideos = asyncHandlers(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+  const userId = req.user._id;
+  const videos = await Video.find({ owner: userId }).sort({ createdAt: -1 });
+  return res
+    .status(200)
+    .json(new ApiResponse(true, "Channel videos fetched successfully", videos));
 });
 
 export { getChannelStats, getChannelVideos };
